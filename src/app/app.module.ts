57d759709656c2e33f2a959e9e7a5a7cec751675
Meze <mezequielabogado@gmail.com>
2020-01-31 23:42:32 -0300

    Inicio projecto cucciolo. Header hardcodeado. Se debe reemplazar por componente con ruta al inicio.

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/compartido/navbar/navbar.component';
+import { HeaderComponent } from './componentes/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
