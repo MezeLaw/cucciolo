2ebf92a8bfc352025f329a262cc8ce976555e1b9
Meze <mezequielabogado@gmail.com>
2020-02-05 20:20:54 -0300

    Correccion de estilos, agregado de efectos, mock primeros paquetes.

--- src/app/servicios/productos.service.ts
+++ src/app/servicios/productos.service.ts
@@ -15,74 +15,53 @@ export class ProductosService {
         { nombre:"Pro Plan 20kg",
           fabricante:"Purina",
           precio: 2600,
-          imgUrl:"https://s3-sa-east-1.amazonaws.com/mispichos.com.ar/mp_images/pro-plan-adult-complete-razas-medianas_143",
+          imgUrl:"https://images-na.ssl-images-amazon.com/images/I/81ks8yWZz3L._AC_SX425_.jpg",
           categoria: "Alimento",
+          descripcion: "Alimento para perro.",
           animal: "Perro" },
 
           { nombre:"Pro Plan 15kg",
           fabricante:"Purina",
-          precio: 2600,
-          imgUrl:"https://s3-sa-east-1.amazonaws.com/mispichos.com.ar/mp_images/pro-plan-adult-complete-razas-medianas_143",
+          precio: 2400,
+          imgUrl:"https://images-na.ssl-images-amazon.com/images/I/81H30BW8SPL._AC_SX679_.jpg",
           categoria: "Alimento",
+          descripcion: "Alimento para perro.",
           animal: "Perro" },
 
           { nombre:"Pro Plan 5kg",
           fabricante:"Purina",
-          precio: 2600,
-          imgUrl:"https://s3-sa-east-1.amazonaws.com/mispichos.com.ar/mp_images/pro-plan-adult-complete-razas-medianas_143",
+          precio: 1540,
+          imgUrl:"//d34zlyc2cp9zm7.cloudfront.net/products/cad32579a3f79e49f5fbedd882228fc58084be962824ff39918c6cea8ae21a02.webp_500",
           categoria: "Alimento",
+          descripcion: "Alimento para perro.",
           animal: "Perro" },
+
+
+          { nombre:"Pro Plan 30kg",
+          fabricante:"Purina",
+          precio: 2600,
+          imgUrl:"https://images-na.ssl-images-amazon.com/images/I/81HmiLLkHJL._AC_SL1500_.jpg",
+          categoria: "Alimento",
+          descripcion: "Alimento para gato.",
+          animal: "Gato" },
 
 
           { nombre:"Pro Plan 20kg",
           fabricante:"Purina",
-          precio: 2600,
-          imgUrl:"https://s3-sa-east-1.amazonaws.com/mispichos.com.ar/mp_images/pro-plan-adult-complete-razas-medianas_143",
+          precio: 2000,
+          imgUrl:"https://images-na.ssl-images-amazon.com/images/I/71Ec3KtvNvL._AC_SL1500_.jpg",
           categoria: "Alimento",
-          animal: "Perro" },
+          descripcion: "Alimento para gato.",
+          animal: "Gato" },
 
 
-          { nombre:"Pro Plan 20kg",
+          { nombre:"Pro Plan 15kg",
           fabricante:"Purina",
-          precio: 2600,
+          precio: 1400,
           imgUrl:"https://s3-sa-east-1.amazonaws.com/mispichos.com.ar/mp_images/pro-plan-adult-complete-razas-medianas_143",
           categoria: "Alimento",
-          animal: "Perro" },
-
-
-          { nombre:"Pro Plan 20kg",
-          fabricante:"Purina",
-          precio: 2600,
-          imgUrl:"https://s3-sa-east-1.amazonaws.com/mispichos.com.ar/mp_images/pro-plan-adult-complete-razas-medianas_143",
-          categoria: "Alimento",
-          animal: "Perro" },
-
-
-          { nombre:"Pro Plan 20kg",
-          fabricante:"Purina",
-          precio: 2600,
-          imgUrl:"https://s3-sa-east-1.amazonaws.com/mispichos.com.ar/mp_images/pro-plan-adult-complete-razas-medianas_143",
-          categoria: "Alimento",
-          animal: "Perro" },
-
-
-          { nombre:"Pro Plan 20kg",
-          fabricante:"Purina",
-          precio: 2600,
-          imgUrl:"https://s3-sa-east-1.amazonaws.com/mispichos.com.ar/mp_images/pro-plan-adult-complete-razas-medianas_143",
-          categoria: "Alimento",
-          animal: "Perro" },
-
-
-          { nombre:"Pro Plan 20kg",
-          fabricante:"Purina",
-          precio: 2600,
-          imgUrl:"https://s3-sa-east-1.amazonaws.com/mispichos.com.ar/mp_images/pro-plan-adult-complete-razas-medianas_143",
-          categoria: "Alimento",
-          animal: "Perro" }
-
-
-
+          descripcion: "Alimento para gato.",
+          animal: "Gato" }
 
     ];
 
@@ -103,5 +82,6 @@ export interface Producto {
   precio:number;
   imgUrl:string;
   categoria: string; 
+  descripcion:string;
   animal?:string;
 }
